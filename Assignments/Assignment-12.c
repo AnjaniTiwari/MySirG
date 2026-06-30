68fd3ddb710428c0d1edf393d7f77151d49e128e
AnjaniTiwari <anjanitiwari001@gmail.com>
2026-06-30 19:50:33 +0530

    Assignment 12 complete.

--- Assignments/Assignment-12.c
+++ Assignments/Assignment-12.c
@@ -79,11 +79,11 @@ void f4() {
     }
 }
 
-//5.
-// * * * * * * * 
-//   * * * * *   
-//     * * *     
-//       *   
+// //5.
+// A B C D E F G 
+//   A B C D E   
+//     A B C     
+//       A   
 void f5() {
     char ch;
     for(int i = 1; i <= 4; ++i) {
@@ -104,20 +104,48 @@ void f5() {
 //     A B A     
 //       A   
 void f6() {
-    int k, flag;
+    char ch;
+    for(int i = 1; i <= 4; ++i) {
+        ch = 'A'-1;
+        for(int j = 1; j <= 7; ++j) {
+            if(j >= i && j <= 8-i) {
+                j <= 4 ? ch++ : --ch;
+                printf("%c ", ch);
+            }
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+}
+
+// //7.
+//          1        
+//        1   1      
+//      1   2   1    
+//    1   2   2   1  
+//  1   2   3   2   1 
+void f7_1() {
+    int k, flag, flag2, p_count, n_count;
     for(int i = 1; i <= 5; ++i) {
-        k = 1;
         flag = 1;
+        flag2 = 0;
+        k = 0;
+        p_count = 0;
+        n_count = i%2 ? (i/2)+1 : i/2;
         for(int j = 1; j <= 9; ++j) {
             if(j >= 6-i && j <= 4+i && flag) {
-                printf("%d ", k);
+                if(!flag2 && k < n_count) 
+                    ++k;
+                else if((2*p_count)-i != 0) {
+                    --k; 
+                    flag2 = 1;
+                } 
+                printf(" %d", k);
+                p_count++;
                 flag = 0;
-                if(j < 5)
-                 ++k;
-                else 
-                    --k;
             }
-            else {
+            else {    
                 printf("  ");
                 flag = 1;
             }
@@ -126,10 +154,104 @@ void f6() {
     }
 }
 
+void f7(int line) {
+    int k, flag;
+    for(int i = 1; i <= line; ++i) {
+        flag = 1;
+        k = 1;
+        for(int j = 1; j <= 2*line-1; ++j) {
+            if(j >= line+1-i && j <= line-1+i && flag) {
+                printf(" %d", k);
+                j < line ? ++k : --k;
+                flag = 0;
+            }
+            else {    
+                printf("  ");
+                if(j == line) --k;
+                flag = 1;
+            }
+        }
+        printf("\n");
+    }
+}
+
+//8.
+// A B C D C B A 
+//   A B C B A   
+//     A B A     
+//       A   
+void f8() {
+    for(int i = 1; i <= 4; ++i) {
+        for(int j = 1; j <= 7; ++j) {
+            if(j <= 5-i || j >= 3+i) {
+                printf("* ");
+            }
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+}
+
+//9.
+// 1 2 3 4 3 2 1 
+// 1 2 3   3 2 1 
+// 1 2       2 1 
+// 1           1  
+void f9() {
+    int k, flag;
+    for(int i = 1; i <= 4; ++i) {
+        k  = 0;
+        flag = 0;
+        for(int j = 1; j <= 7; ++j) {
+            if(!flag && k < 4)
+                ++k;
+            else {
+                --k;
+                flag = 1;
+            }
+            if(j <= 5-i || j >= 3+i) {
+                printf("%d ", k);
+            }
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+}
+
+//10.
+// A B C D C B A 
+// A B C   C B A 
+// A B       B A 
+// A           A  
+void f10() {
+    char k; 
+    int flag;
+    for(int i = 1; i <= 4; ++i) {
+        k  = 'A'-1;
+        flag = 0;
+        for(int j = 1; j <= 7; ++j) {
+            if(!flag && k < 'A'+3)
+                ++k;
+            else {
+                --k;
+                flag = 1;
+            }
+            if(j <= 5-i || j >= 3+i) {
+                printf("%c ", k);
+            }
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+}
+
 #include <stdio.h>
 int main() {
     system("clear");
-    f6();
+    f7(10);
     printf("\n");
     return 0;
 }
