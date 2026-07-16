59a3a182e629fcb2cece0cef027b69c901d9e20d
AnjaniTiwari <anjanitiwari001@gmail.com>
2026-07-16 17:40:32 +0530

    Assinment 14 completed.

--- Assignments/Assignment-14-function.c
+++ Assignments/Assignment-14-function.c
@@ -1,72 +1,88 @@
 #include <stdio.h>
 // Q1
-float circle_area(int radius) {
-    return 3.14*radius*radius;
+float circle_area(int radius)
+{
+    return 3.14 * radius * radius;
 }
 // Q2
-float simple_interest(int principal, int interest_rate, int time) {
-    return (principal*interest_rate*time)/100.0;
+float simple_interest(int principal, float interest_rate, int time)
+{
+    return (principal * interest_rate * time) / 100.0;
 }
 // Q3
-int is_even(int num) {
-    return num&1 ? 0 : 1; 
+int is_even(int num)
+{
+    return num & 1 ? 0 : 1;
 }
 // Q4
-void natural_number(int num) {
-    for(int i = 1; i<=num; ++i)
-        printf("%d ", i);;
-}
-//Q5
-void odd_natural_number(int num) {
-    for(int i = 1; i<=num*2; i+=2)
+void natural_number(int num)
+{
+    for (int i = 1; i <= num; ++i)
         printf("%d ", i);
 }
-//Q6
-int factorial(int num) {
-    if(num == 1)
-        return 1;
-    return num*factorial(num-1);
+// Q5
+void odd_natural_number(int num)
+{
+    for (int i = 1; i <= num * 2; i += 2)
+        printf("%d ", i);
 }
+// Q6
+int factorial(int num)
+{
+    int fact = 1;
+    while(num) {
+        fact*=num;
+        --num;
+    }
+    return fact;
+}
+
 //Q7
 int combination(int n, int r) {
-    int c, j, pair = 0;
-    int last_j;
-    // x = r&1 ? n-1 : n;
-    for(int i = 1; i <= n && n-i >= r-1; ++i) {
-        c = 0;
-        j = last_j = i;
-        while(j <= n) {
-            if(c == 0) {
-                if(j == n && r%2 != 0)
-                    break;
-                printf("\n%d ", i);
-                j = ++last_j;
-                c++;
-            }
-            printf("%d ", j);
-            c++;
-            if(c != r) {
-                j++;
-            }
-            else {
-                c = 0;
-                if(r%2 == 0)
-                    j++;
-            }
-        } 
+    return factorial(n)/ factorial(r) * factorial(n-r);
+}
+
+//Q8
+int permutation(int n, int r) {
+    return factorial(n)/factorial(n-r);
+}
+
+//Q9
+int is_number_contains_digit(int num, int digit) {
+    while(num) {
+        if(num%10 == digit)
+            return 1;
+        num/=10;
+    }
+    return 0;
+}
+
+//Q10
+void print_all_prime_factors(int num) {
+    int d = 2;
+    while(num != 1) {
+        if(num%d == 0) {
+            printf("%d ", d);
+            num/=d;
+        }
+        else
+            d++;
     }
 }
 
-
-int main() {
+int main()
+{
     system("clear");
     // printf("%f", circle_area(3));
     // printf("%f", simple_interest(10000, 5, 2));
     // printf("%s", is_even(24) ? "Even" : "Odd");
     // natural_number(10);
     // odd_natural_number(10);
-    // printf("%d", factorial(5));
-    combination(5, 4);
+    // printf("%d", factorial(3));
+    // printf("%d", combination(3, 2));
+    // printf("%d ", permutation(3, 2));
+    // printf("%d ", is_number_contains_digit(39871, 2));
+    print_all_prime_factors(75);
     printf("\n");
     return 0;
 }
