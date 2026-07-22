4ab66d526ef3c0d62a80adf431647afa073d2d64
AnjaniTiawri <anjanitiwari001@gmail.com>
2026-07-22 19:58:35 +0530

    C++ inline and default argument use learn.

---
+++ CPP-Programs/first.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+inline int square(int num) {
+    return num*num;
+}
+
+inline int add(int n1, int n2, int n3 = 0) {
+    return n1+n2+n3;
+}
+using namespace std;
+int main() {
+    // int x;
+    // cout << "Enter a number ";
+    // cin >> x;
+    // cout << "Square of " << x << " is " << square(x) << endl;
+    
+    int x, y;
+    cout << "Enter two number ";
+    cin >> x >> y;
+    cout <<  add(x, y) << endl;
+    
+    int z;
+    cout << "\nEnter three number ";
+    cin >> x >> y >> z;
+    cout <<  add(x, y, z) << endl;
+    return 0;
+}
