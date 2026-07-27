0d47d306f78becfb6e4a4f829ae43c2486987aa2
AnjaniTiawri <anjanitiwari001@gmail.com>
2026-07-27 19:57:41 +0530

    learnning Encapsulation, data hiding and abstration.

--- structure.cpp
+++ structure.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-// #include <string>
 
 using namespace std;
 
@@ -7,25 +6,26 @@ struct book {
     int bookid;
     char title[20];
     float price;
+    
+    void input() {
+        cout << "Enter bookid, title and price: ";
+        cin >> bookid;
+        cin.ignore();
+        cin.getline(title, sizeof(title));
+        cin >> price;
+    }
+
+    inline void display() {
+        cout << bookid << " " << title << " " << price << endl;    
+    }
 };
 
-void input(struct book* b) {
-    cout << "Enter bookid, title and price: ";
-    cin >> b->bookid;
-    cin.ignore();
-    cin.getline(b->title, sizeof(b->title));
-    cin >> b->price;
-}
 
-
-void display(struct book* b) {
-    cout << b->bookid << " " << b->title << " " << b->price << endl; 
-}
 
 int main() {
-    struct book b;
-    input(&b);
-    display(&b);
+    book b;
+    b.input();
+    b.display();
     cout << endl;
     return 0;
 }
