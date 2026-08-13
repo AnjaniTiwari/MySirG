dfa2c8c9020a60292722dfadd1b21e505af52320
AnjaniTiawri <anjanitiwari001@gmail.com>
2026-08-13 18:23:15 +0530

    Assignment-25-pointer completed.

--- C-Programs/Assignments/Assignment-25-pointer.c
+++ C-Programs/Assignments/Assignment-25-pointer.c
@@ -49,12 +49,12 @@ void extract_string(char* str, int start_index, int end_index, char* result) {
         while(start_index != end_index) {
             result[i++] = str[start_index++];
         }
+        result[i] = '\0';
         printf("%s", result);
     }
     else {
         printf("index out of bound.\n");
     }
-
 }
 
 //Q6
@@ -103,39 +103,40 @@ print_arr(int* arr, int size) {
     printf("\n");
 }
 void move_first_index(int* arr, int size) {
-    int i, j, flag;
-    int m = size/2;
+    int i, x, temp;
+    int m = 0;
     
     print_arr(arr, size);
-    swap(&arr[0], &arr[m]);
 
-    j = m+1;
-    for(i = m-1; i >= 0; --i)  {
-        flag = 0;
-        if(arr[i] > arr[m]) {
-            while(j < size) {
-                if(arr[j] < arr[m]) {
-                    swap(&arr[i], &arr[j]);
-                    flag = 1;
-                    break;
-                }
-                j++;
-            }
-            if(j == size) {
-                swap(&arr[i], &arr[m]);
-                m = i;
-            }
+    for(i = m+1; i < size; ++i) {
+        if(arr[m] >= arr[i]) {
+            temp = arr[i];
+            for(x = i; x > m; --x) 
+                arr[x] = arr[x-1];
+            arr[m] = temp;
+            m++;
         }
     }
-    while(j < size) {
-        if(arr[j] < arr[m]) {
-            swap(&arr[j], &arr[m]);
-            m = j;
-        }
-        j++;
-    }
+    
     print_arr(arr, size);
 
+}
+
+//Q10
+int highest_marks(int** class, int n_class, int* len) {
+    int max = class[0][0];
+    int i = 0, j = 0;
+    
+    while(i < n_class) {
+        j = 0;
+        while(j < len[i]) {
+            if(max < class[i][j])
+                max = class[i][j];
+            ++j;
+        }
+        ++i; 
+    }
+    return max;
 }
 
 int main() {
@@ -143,9 +144,23 @@ int main() {
     int n1 = 10, n2 = 20;
     char arr[20] = "AXY";
     char str[] = "Programming";
-    int num[] = {5, 8, 2, 7, 3, 9, 1};
-    int num2[] = {8, 9, 7, 0, 10};
+    int num[] = {23, 67, 89, 11, 23, 35, 42, 59, 16, 8, 81};
+    int num2[] = {5, 4, 3, 2, 1};
     int num3[20] = {0};
+    
+    
+    int class1_student_marks[3] = {21, 24, 29};
+    int class2_student_marks[2] = {22, 25};
+    int class3_student_marks[1] = {30};
+    int class4_student_marks[2] = {19, 23};
+    int class5_student_marks[3] = {26, 27, 19};
+    int* class[5] = {class1_student_marks,
+                     class2_student_marks,
+                     class3_student_marks,
+                     class4_student_marks,
+                     class5_student_marks};
+
+    int student_marks_arr_len[] = {3, 2, 1, 2, 3};
 
     // swap(&n1, &n2);
     // printf("%d %d", n1, n2);
@@ -158,6 +173,7 @@ int main() {
     // sort(num, sizeof(num)/sizeof(*num));
     // marge(num, sizeof(num)/sizeof(*num), num2, sizeof num2/sizeof *num2, num3);
     move_first_index(num, sizeof(num)/sizeof(*num));
+    // printf("Highest mark - %d", highest_marks(class, 5, student_marks_arr_len));
     printf("\n");
     return 0;
 }
